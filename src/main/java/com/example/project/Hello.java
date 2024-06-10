f6dbbfee9f69988b578e2677add5566abc6f8054
toseman <toseman@lamar.edu>
2024-06-10 08:29:30 -0500

    Update Hello.java
    changed messaged, added final back in

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -1,10 +1,9 @@
 package com.example.project;
 
-public class Hello {
+public class Hello{
 
-	public static void main(String[] args) {
-		System.out.println("Hello");
+	public static void main(final String[] args){
+		System.out.println("Hello Java");
 	}
-
 }
 
