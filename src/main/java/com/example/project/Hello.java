bc0611910ef528ce4236dfae4975e55f062b32f0
toseman <toseman@lamar.edu>
2024-06-10 09:40:48 -0500

    Update Hello.java

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -1,7 +1,10 @@
 package com.example.project;
 
 public class Hello{
-	public static void main(final String[] args){
-		System.out.println("Not the right string, tests will fail!");
+	public static void main(String[] args){
+		
+		System.out.println("Hello Java");
+		
 	}
+	
 }
